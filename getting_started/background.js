4c1afb818a7e0901d4adb52beb5d6694aed924ac
corselia <takiya@toran.sakura.ne.jp>
2020-02-09 00:45:45 +0900

    "onInstalled" メソッドを使った場合のデバッグについて注釈を追加した

--- getting_started/background.js
+++ getting_started/background.js
@@ -1,3 +1,5 @@
+// onInstalled のところでエラーが出ていたと仮定してそれを修正した場合、
+// その修正の確認は「アンインストール」→「再インストール」という過程を経なければ確認できないことには注意する
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({ color: '#3aa757' }, function() {
     console.log('The color is green.');
